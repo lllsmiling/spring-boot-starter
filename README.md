3f118cc5639df8e54a27f66a1a3de5fe440508a2
lllsmiling <lllsmiling@126.com>
2018-07-19 14:14:15 +0800

    response msg test api

--- README.md
+++ README.md
@@ -1,5 +1,91 @@
 # spring-boot-starter
 spring-boot-starter mybatits durid+durid monitor
 
-定义通用response enum
-使用druid数据源
+## api response
+通用返回
+
+    /**
+     * common success response
+     * @return success json
+     */
+    @RequestMapping(value = "common", method = RequestMethod.GET)
+    public ResModel common() {
+        return ResUtils.common(ResEnum.success);
+    }
+返回示例
+
+    {
+    	"code":200,
+    	"msg":"成功",
+    	"state":true
+    }
+
+自定义消息
+
+    /**
+     * common success and custom msg
+     * @return
+     */
+    @RequestMapping(value = "commonMsg", method = RequestMethod.GET)
+    public ResModel commonMsg() {
+        return ResUtils.commonMsg(ResEnum.success,"successMsg");
+    }
+返回示例  
+      
+    {
+    	"code":200,
+    	"msg":"successMsg",
+    	"state":true
+    }
+
+返回数据
+
+    /**
+     * response data
+     * @return
+     */
+    @RequestMapping(value = "data", method = RequestMethod.GET)
+    public ResModel data() {
+        Map<String,Object> result = Maps.newHashMap();
+        result.put("first","Hello World");
+        return ResUtils.data(ResEnum.success,result);
+    }
+
+返回示例
+    
+    {
+    	"code":200,
+    	"data":{
+    		"first":"Hello World"
+    	},
+    	"msg":"成功",
+    	"state":true
+    }
+        
+返回数据，并自定义消息
+
+    /**
+     * response data and custom msg
+     * @return
+     */
+    @RequestMapping(value = "dataMsg", method = RequestMethod.GET)
+    public ResModel dataMsg() {
+        Map<String,Object> result = Maps.newHashMap();
+        result.put("first","Hello World");
+        return ResUtils.dataMsg(ResEnum.success,result,"dataMsg");
+    }
+    
+返回示例：
+
+    {
+    	"code":200,
+    	"data":{
+    		"first":"Hello World"
+    	},
+    	"msg":"dataMsg",
+    	"state":true
+    }
+
+简化复杂的自定义消息，具体定制消息可以在net.ailq.response配置
+    
+    
--- src/main/java/net/ailq/controller/TestController.java
+++ src/main/java/net/ailq/controller/TestController.java
@@ -1,12 +1,66 @@
 package net.ailq.controller;
 
+import com.google.common.collect.Maps;
+import net.ailq.response.ResEnum;
+import net.ailq.response.ResModel;
+import net.ailq.response.ResUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author lllsmiling
  * @create 2018-07-16 11:00
  **/
-@Controller
+@RestController
+@RequestMapping("test/response")
 public class TestController {
+    /**
+     * common success response
+     * @return success json
+     */
+    @RequestMapping(value = "common", method = RequestMethod.GET)
+    public ResModel common() {
+        return ResUtils.common(ResEnum.success);
+    }
+
+    /**
+     * common success and custom msg
+     * @return
+     */
+    @RequestMapping(value = "commonMsg", method = RequestMethod.GET)
+    public ResModel commonMsg() {
+        return ResUtils.commonMsg(ResEnum.success,"successMsg");
+    }
+
+
+    /**
+     * response data
+     * @return
+     */
+    @RequestMapping(value = "data", method = RequestMethod.GET)
+    public ResModel data() {
+        Map<String,Object> result = Maps.newHashMap();
+        result.put("first","Hello World");
+        return ResUtils.data(ResEnum.success,result);
+    }
+
+
+    /**
+     * response data and custom msg
+     * @return
+     */
+    @RequestMapping(value = "dataMsg", method = RequestMethod.GET)
+    public ResModel dataMsg() {
+        Map<String,Object> result = Maps.newHashMap();
+        result.put("first","Hello World");
+        return ResUtils.dataMsg(ResEnum.success,result,"dataMsg");
+    }
+
 
 }
